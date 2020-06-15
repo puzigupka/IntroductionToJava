4fbbb5a37c62961dd76496a1b5e2f1c1e2aeecb9
puzigupka <57733107+puzigupka@users.noreply.github.com>
2020-06-16 00:27:59 +0300

    Test2

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,9 +2,17 @@ import org.junit.Test;
 
 public class MainClass
 {
+    private int class_number = 20;
+
+    public int getClassNumber()
+    {
+        return class_number*3;
+    }
+
     public int getLocalNumber(int number)
     {
         return number = 14;
     }
+
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,15 @@ public class MainClassTest
         int a = Main.getLocalNumber(14);
     }
 
+    @Test
+    public void testGetClassNumber()
+    {
+        int a = Main.getClassNumber();
+        if (a > 45){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+    }
+
 }
