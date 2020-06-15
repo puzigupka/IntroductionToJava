6420fb0de1d2357a88ecc117e1b6566eadb032c5
puzigupka <57733107+puzigupka@users.noreply.github.com>
2020-06-16 02:17:46 +0300

    Test2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,25 +4,25 @@ import org.junit.Test;
 public class MainClassTest
 {
     MainClass Main = new MainClass();
-    @Test
-    public void myFirstTest()
-    {
-        int a = Main.getLocalNumber(14);
-        Assert.assertTrue("the method does not return a number",a == 14);
-    }
-
 //    @Test
-//    public void testGetClassNumber()
+//    public void myFirstTest()
 //    {
-//        int a = Main.getClassNumber();
-//        int b = 45;
-//
-//        Assert.assertTrue("20 * 3 <= 45",a > b);
-//        if (a > 45){
-//            System.out.println("True");
-//        }else {
-//            System.out.println("False");
-//       }
+//        int a = Main.getLocalNumber(14);
+//        Assert.assertTrue("the method does not return a number",a == 14);
+//    }
+
+    @Test
+    public void testGetClassNumber() {
+        int a = Main.getClassNumber();
+        int b = 45;
+
+        Assert.assertTrue("20 * 3 <= 45", a > b);
+        if (a > 45) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
 
 
 }
