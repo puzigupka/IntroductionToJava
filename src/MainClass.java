15cf4bbb4549df98bb5aeb0add1e217c9a5812a8
puzigupka <57733107+puzigupka@users.noreply.github.com>
2020-06-16 02:13:30 +0300

    Test1

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,16 +3,24 @@ import org.junit.Test;
 public class MainClass
 {
     private int class_number = 20;
+    private String class_string = "Hello, world";
 
     public int getClassNumber()
     {
+        System.out.println("60>45");
         return class_number*3;
     }
 
-    /*public int getLocalNumber(int number)
+    public int getLocalNumber(int number)
     {
+        System.out.println("a = 14");
         return number = 14;
-    }*/
+    }
+
+    public String getClassString()
+    {
+        return class_string;
+    }
 
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,23 +1,28 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest
 {
     MainClass Main = new MainClass();
-    /*@Test
+    @Test
     public void myFirstTest()
     {
         int a = Main.getLocalNumber(14);
-    }*/
-
-    @Test
-    public void testGetClassNumber()
-    {
-        int a = Main.getClassNumber();
-        if (a > 45){
-            System.out.println("True");
-        }else {
-            System.out.println("False");
-        }
+        Assert.assertTrue("the method does not return a number",a == 14);
     }
+
+//    @Test
+//    public void testGetClassNumber()
+//    {
+//        int a = Main.getClassNumber();
+//        int b = 45;
+//
+//        Assert.assertTrue("20 * 3 <= 45",a > b);
+//        if (a > 45){
+//            System.out.println("True");
+//        }else {
+//            System.out.println("False");
+//       }
+
 
 }
